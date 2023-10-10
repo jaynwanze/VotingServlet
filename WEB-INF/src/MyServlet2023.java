64715247f73d70bc16af6ef99b5348b3b0079116
“Jay” <j.nwanze@gmail.com>
2023-10-10 15:28:26 +0100

    Updating code to take in 3 seperate inputs for seperate songs

--- WEB-INF/src/MyServlet2023.java
+++ WEB-INF/src/MyServlet2023.java
@@ -13,11 +13,14 @@ public class MyServlet2023 extends HttpServlet {
 
 		String email = request.getParameter("email");
 		String name = request.getParameter("name");
-		String favSongs = request.getParameter("top3FavSongs");
+		String song1 = request.getParameter("song1");
+		String song2 = request.getParameter("song2");
+		String song3 = request.getParameter("song3");
+
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("<html><body><h1>Thanks for your vote!</h1><p><h3>Here are you details!:</h3>Email: "+ email + "<br>Name: " + name + "<br>Top 3 Favourite Songs of All Time: " + favSongs + " </p></body></html>");
+		out.println("<html><body><h1>Thanks for your vote!</h1><p><h3>Here are you details!:</h3>Email: "+ email + "<br>Name: " + name + "<br>Top 3 Favourite Songs of All Time: " + song1 + ", " + song2 + " and  " + song3 +" </p></body></html>");
 
 	}
 
